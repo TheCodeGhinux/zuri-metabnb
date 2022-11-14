ddb8d55ec1254faccbb5a19012115c0c008ba92f
giy21 <ilesanmibry@gmail.com>
2022-11-14 09:10:47 +0100

    Added nav links

--- src/location/Location.jsx
+++ src/location/Location.jsx
@@ -8,11 +8,7 @@ import img5 from '../assets/home-pic/Frame 155.png'
 import img6 from '../assets/home-pic/Frame 156.png'
 import img7 from '../assets/home-pic/Frame 157.png'
 import img8 from '../assets/home-pic/Frame 158.png'
-import styled from "styled-components";
-import love from '../assets/love.svg'
-import love1 from '../assets/vector.svg'
-import {FiHeart} from 'react-icons/fi'
-import {BsSuitHeart} from 'react-icons/bs'
+
 
 
 
